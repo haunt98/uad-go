37fb9635ca8ae2c6c4424af98503d8e72ea3e08c
Hau Nguyen <hauvipapro@gmail.com>
2023-08-28 00:45:07 +0700

    feat: embed json

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+)
+
+//go:embed uad_lists.json
+var uadBytes []byte
+
 func main() {
+	apps := Apps{}
+	if err := json.Unmarshal(uadBytes, &apps); err != nil {
+		slog.Error("json: failed to unmarshal apps", err)
+		return
+	}
+
+	fmt.Println(len(apps))
 }
