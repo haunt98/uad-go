8380b302580b006bb9eba6024074bc0434d9784c
Hau Nguyen <hauvipapro@gmail.com>
2023-09-05 17:03:04 +0700

    chore: fix lint

--- models.go
+++ models.go
@@ -9,10 +9,10 @@ type UADApp struct {
 	ID           string   `json:"id,omitempty"`
 	List         string   `json:"list,omitempty"`
 	Description  string   `json:"description,omitempty"`
+	Removal      string   `json:"removal,omitempty"`
 	Dependencies []string `json:"dependencies,omitempty"`
 	NeededBy     []string `json:"neededBy,omitempty"`
 	Labels       []string `json:"labels,omitempty"`
-	Removal      string   `json:"removal,omitempty"`
 }
 
 // https://github.com/MuntashirAkon/android-debloat-list
